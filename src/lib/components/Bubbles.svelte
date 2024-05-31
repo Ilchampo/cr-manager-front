186715b5e4f96d3d291ce9bd4b930e9d12851c1c
Pablo Beltran <beltranflores.juan@gmail.com>
2024-05-31 11:20:20 -0500

    Added homepage view

---
+++ src/lib/components/Bubbles.svelte
@@ -0,0 +1,95 @@
+<div class="flex flex-col h-full overflow-hidden">
+	<svg
+		class="svg-icon circle fill-secondary-600"
+		id="circle1"
+		width="400"
+		height="400"
+	>
+		<circle cx="200" cy="200" r="200" />
+	</svg>
+
+	<svg
+		class="svg-icon circle fill-secondary-500"
+		id="circle2"
+		width="300"
+		height="300"
+	>
+		<circle cx="150" cy="150" r="150" />
+	</svg>
+
+	<svg
+		class="svg-icon circle fill-secondary-400"
+		id="circle3"
+		width="250"
+		height="250"
+	>
+		<circle cx="125" cy="125" r="125" />
+	</svg>
+
+	<svg
+		class="svg-icon circle fill-secondary-300"
+		id="circle4"
+		width="200"
+		height="200"
+	>
+		<circle cx="100" cy="100" r="100" />
+	</svg>
+
+	<svg
+		class="svg-icon circle fill-secondary-200"
+		id="circle5"
+		width="100"
+		height="100"
+	>
+		<circle cx="50" cy="50" r="40" />
+	</svg>
+</div>
+
+<style>
+	.circle {
+		position: absolute;
+		animation: float 10s infinite ease-in-out;
+	}
+
+	#circle1 {
+		top: 10%;
+		left: 10%;
+		animation-duration: 8s;
+	}
+
+	#circle2 {
+		top: 60%;
+		left: 30%;
+		animation-duration: 6s;
+	}
+
+	#circle3 {
+		top: 30%;
+		left: 50%;
+		animation-duration: 10s;
+	}
+
+	#circle4 {
+		top: 60%;
+		left: 70%;
+		animation-duration: 7s;
+	}
+
+	#circle5 {
+		top: 45%;
+		left: 80%;
+		animation-duration: 9s;
+	}
+
+	@keyframes float {
+		0% {
+			transform: translateY(0) translateX(0);
+		}
+		50% {
+			transform: translateY(-50px) translateX(20px);
+		}
+		100% {
+			transform: translateY(0) translateX(0);
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Bubbles from '$lib/components/Bubbles.svelte';
+	import images from '$lib/assets/images';
+
+	import { goto } from '$app/navigation';
+</script>
+
+<div
+	class="flex flex-col w-screen h-screen bg-gradient-to-r from-primary-900 to-primary-500 relative"
+>
+	<div class="flex flex-col h-full w-full absolute">
+		<div class="grid grid-cols-1 md:grid-cols-2 z-10 h-full">
+			<div class="flex flex-col gap-8 justify-center items-start p-12 h-full">
+				<h1
+					class="h1 text-surface-100 font-semibold tracking-wide text-5xl md:text-7xl"
+				>
+					Planning project
+					<span class="gradient-tertiary font-bold">releases</span> was never
+					this
+					<span class="gradient-tertiary font-bold">easy</span>.
+				</h1>
+				<div class="flex gap-4 justify-start items-center">
+					<button
+						class="btn variant-filled-primary justify-center items-center w-32"
+						on:click={() => goto('/auth')}
+					>
+						Sign in
+					</button>
+					<button
+						class="btn variant-filled-surface justify-center items-center w-32"
+						on:click={() => goto('/auth/signup')}
+					>
+						Sign up
+					</button>
+				</div>
+			</div>
+			<div class="hidden md:flex flex-col h-full justify-center items-center">
+				<img src={images.DEMO_USERS} alt="Demo users" />
+			</div>
+		</div>
+	</div>
+	<div class="flex flex-col h-full w-full relative opacity-50 z-0">
+		<Bubbles />
+	</div>
+</div>
